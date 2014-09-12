3cf86b4c615cebd06e350751fdbbb0deade7f063
Sagiv Ofek <sagiv4@gmail.com>
2014-09-11 23:27:22 -0400

    Update examples.js

--- examples.js
+++ examples.js
@@ -4,7 +4,7 @@ var ac = new acceptBitcoin('YOUR_BITCOIN_ADDRESS', settings);
 key = ac.generateAddress({alertWhenHasBalance: true});
 console.log("Hello buyer! please pay to: " + key.address());
 key.on('hasBalance', function(amount){
-  console.log "thanks for paying me " + amount; //do stuff
+  console.log("thanks for paying me " + amount); //do stuff
   key.transferBalanceToMyAccount(function(err, d){
     if (d.status === 'success') console.log("Cool, the bitcoins are in my private account!");
   });
